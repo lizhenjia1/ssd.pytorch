c93734c9e12affe85154c044607c26bbac656cc9
chensonglu <chenslvs@163.com>
2019-12-01 10:02:00 +0800

    little change

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -9,6 +9,10 @@ import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
+
+import sys
+sys.path.append(".")
+
 from data import VOC_ROOT, VOCAnnotationTransform, VOCDetection, BaseTransform
 from data import VOC_CLASSES as labelmap
 import torch.utils.data as data
