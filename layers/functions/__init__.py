760f2eccdd1a956e10545b3a611996fd71c5c3dd
chensonglu <chenslvs@163.com>
2020-11-22 09:33:24 +0800

    init

--- layers/functions/__init__.py
+++ layers/functions/__init__.py
@@ -2,6 +2,8 @@ from .detection import Detect, Detect_offset, Detect_four_corners, Detect_only_f
 from .prior_box import PriorBox
 from .prior_box_2 import PriorBox_2
 from .prior_box_textboxes import PriorBox_textboxes
+from .prior_box_kmeans import PriorBox_kmeans
 
 
-__all__ = ['Detect', 'Detect_offset', 'Detect_four_corners', 'Detect_only_four_corners', 'PriorBox', 'PriorBox_2', 'PriorBox_textboxes']
+__all__ = ['Detect', 'Detect_offset', 'Detect_four_corners', 'Detect_only_four_corners',
+'PriorBox', 'PriorBox_2', 'PriorBox_textboxes', 'PriorBox_kmeans']
